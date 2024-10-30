442dbd149fbe70cc0bb2625d02474bec1009cccb
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-30 17:39:40 +0100

    feat: init P2d class

--- ebike-service/src/main/java/it/unibo/sap/ass02/demo/domain/P2d.java
+++ ebike-service/src/main/java/it/unibo/sap/ass02/demo/domain/P2d.java
@@ -1,2 +1,41 @@
-package it.unibo.sap.ass02.demo.domain;public class P2d {
+package it.unibo.sap.ass02.demo.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ *
+ * 2-dimensional point
+ * objects are completely state-less
+ *
+ */
+public class P2d implements java.io.Serializable {
+    private final double x;
+    private final double y;
+
+    public P2d(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public double x() {
+        return x;
+    }
+
+    public double y() {
+        return y;
+    }
+
+    public P2d sum(V2d v){
+        return new P2d(x+v.x(),y+v.y());
+    }
+
+    public V2d sub(P2d v){
+        return new V2d(x-v.x(),y-v.y());
+    }
+
+    public String toString(){
+        return "P2d("+x+","+y+")";
+    }
+
 }
