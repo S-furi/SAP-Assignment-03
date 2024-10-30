f78c4a387ce911fae48ed13f63c4248267953110
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-30 17:54:44 +0100

    feat: init EBike controller, init also get all ebikes

--- ebike-service/src/main/java/it/unibo/sap/ass02/demo/controller/EBikeController.java
+++ ebike-service/src/main/java/it/unibo/sap/ass02/demo/controller/EBikeController.java
@@ -1,2 +1,20 @@
-package it.unibo.sap.ass02.demo.controller;public class EBikeController {
+package it.unibo.sap.ass02.demo.controller;
+
+import it.unibo.sap.ass02.demo.domain.EBikeImpl;
+import it.unibo.sap.ass02.demo.repositories.EBikeRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/ebike")
+public class EBikeController {
+    private final EBikeRepository repository;
+    public EBikeController(final EBikeRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/all")
+    public Iterable<EBikeImpl> getAllEBikes() {
+        return this.repository.findAll();
+    }
 }
