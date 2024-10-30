4e1f2d4fdff2f450aed8ce46bc74912abc25c8d6
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-30 17:39:57 +0100

    feat: create EBike implementation

--- ebike-service/src/main/java/it/unibo/sap/ass02/demo/domain/EBikeImpl.java
+++ ebike-service/src/main/java/it/unibo/sap/ass02/demo/domain/EBikeImpl.java
@@ -1,5 +1,10 @@
 package it.unibo.sap.ass02.demo.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Objects;
+
 public class EBikeImpl implements EBike<String>{
     private final String id;
     private EBikeState state;
@@ -11,68 +16,111 @@ public class EBikeImpl implements EBike<String>{
     public EBikeImpl(String id) {
         this(id, EBikeState.AVAILABLE, new P2d(0, 0), 0, 100);
     }
+
+    public EBikeImpl(String id,
+                     EBikeState state,
+                     P2d loc,
+                     double speed,
+                     int batteryLevel) {
+        this.id = id;
+        this.state = state;
+        this.loc = loc;
+        this.direction = new V2d(1,0);;
+        this.speed = speed;
+        this.batteryLevel = batteryLevel;
+    }
+
     @Override
-    public String getID() {
-        return null;
+    public String getId() {
+        return id;
     }
 
     @Override
     public EBikeState getState() {
-        return null;
+        return state;
     }
 
     @Override
     public void rechargeBattery() {
-
+        batteryLevel = 100;
+        state = EBikeState.AVAILABLE;
     }
 
     @Override
     public int getBatteryLevel() {
-        return 0;
+        return batteryLevel;
     }
 
     @Override
     public void decreaseBatteryLevel(int delta) {
-
+        batteryLevel -= delta;
+        if (batteryLevel < 0) {
+            batteryLevel = 0;
+            state = EBikeState.MAINTENANCE;
+        }
+        // NOTE: call the persistence layer
     }
+
 
     @Override
     public boolean isAvailable() {
-        return false;
+        return state.equals(EBikeState.AVAILABLE);
     }
 
     @Override
     public void updateState(EBikeState state) {
-
+        this.state = state;
     }
 
     @Override
     public void updateLocation(P2d newLoc) {
-
+        loc = newLoc;
     }
 
     @Override
     public void updateSpeed(double speed) {
-
+        this.speed = speed;
     }
 
     @Override
     public void updateDirection(V2d dir) {
-
+        this.direction = dir;
     }
 
     @Override
     public double getSpeed() {
-        return 0;
+        return speed;
     }
 
     @Override
     public V2d getDirection() {
-        return null;
+        return direction;
     }
 
     @Override
-    public P2d getLocation() {
-        return null;
+    public P2d getLocation(){
+        return loc;
+    }
+
+    public String toString() {
+        return "{ id: " + id + ", loc: " + loc + ", batteryLevel: " + batteryLevel + ", state: " + state + " }";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EBikeImpl eBike = (EBikeImpl) o;
+        return Objects.equals(id, eBike.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String getID() {
+        return this.id;
     }
 }
