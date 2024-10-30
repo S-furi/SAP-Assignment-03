e356f9c9e99729857f5d41ac47d94d3ead0aaf15
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-10-30 17:39:48 +0100

    feat: init EBike interface

--- ebike-service/src/main/java/it/unibo/sap/ass02/demo/domain/EBike.java
+++ ebike-service/src/main/java/it/unibo/sap/ass02/demo/domain/EBike.java
@@ -1,6 +1,45 @@
 package it.unibo.sap.ass02.demo.domain;
 
 
-public interface EBike {
+public interface EBike<K> extends Entity<K>{
+    K getId();
+    EBikeState getState();
 
+    void rechargeBattery();
+
+    int getBatteryLevel();
+
+    void decreaseBatteryLevel(int delta);
+
+    boolean isAvailable();
+
+    void updateState(EBikeState state);
+
+    void updateLocation(P2d newLoc);
+
+    void updateSpeed(double speed);
+
+    void updateDirection(V2d dir);
+
+    double getSpeed();
+
+    V2d getDirection();
+
+    P2d getLocation();
+
+    enum EBikeState {
+        AVAILABLE,
+        IN_USE,
+        MAINTENANCE;
+
+        public static EBikeState fromValue(String value) {
+            // Normalize the string (e.g., lowercase) and map it to the enum
+            return switch (value.toUpperCase()) {
+                case "AVAILABLE" -> AVAILABLE;
+                case "IN_USE" -> IN_USE;
+                case "MAINTENANCE" -> MAINTENANCE;
+                default -> throw new IllegalArgumentException("Unknown value: " + value);
+            };
+        }
+    }
 }
