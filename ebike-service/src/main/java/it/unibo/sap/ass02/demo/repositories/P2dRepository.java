b7524bef19540dda50030d33f5c564aa9dfce6f9
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-01 10:42:39 +0100

    feat: create a new Repository for the p2d class

--- ebike-service/src/main/java/it/unibo/sap/ass02/demo/repositories/P2dRepository.java
+++ ebike-service/src/main/java/it/unibo/sap/ass02/demo/repositories/P2dRepository.java
@@ -1,2 +1,11 @@
-package it.unibo.sap.ass02.demo.repositories;public interface P2dRepository {
+package it.unibo.sap.ass02.demo.repositories;
+
+import it.unibo.sap.ass02.demo.domain.P2d;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface P2dRepository extends JpaRepository<P2d, Long> {
+    Optional<P2d> findByXAndY(final double x, final double y);
 }
