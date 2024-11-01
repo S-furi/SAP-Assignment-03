379f383a2b52b484ca2255925b21bd532f4fbdb2
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-01 10:48:59 +0100

    feat(controller): add new implementation for the getAllBike by using the service method

--- ebike-service/src/main/java/it/unibo/sap/ass02/demo/controller/EBikeController.java
+++ ebike-service/src/main/java/it/unibo/sap/ass02/demo/controller/EBikeController.java
@@ -2,44 +2,48 @@ package it.unibo.sap.ass02.demo.controller;
 
 import it.unibo.sap.ass02.demo.domain.EBikeImpl;
 import it.unibo.sap.ass02.demo.repositories.EBikeRepository;
+import it.unibo.sap.ass02.demo.service.EBikeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
 import java.util.Objects;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/ebike")
 public class EBikeController {
-    private final EBikeRepository repository;
-    public EBikeController(final EBikeRepository repository) {
-        this.repository = repository;
+    private final EBikeService eBikeService;
+    public EBikeController(final EBikeService eBikeService) {
+        this.eBikeService = eBikeService;
     }
 
     @GetMapping("/all")
     public Iterable<EBikeImpl> getAllEBikes() {
-        return this.repository.findAll();
+        return this.eBikeService.getAllEBike();
     }
 
     @GetMapping("/{id}")
     public Optional<EBikeImpl> getEBikeByID(@PathVariable String id) {
         if (Objects.nonNull(id)) {
-            return this.repository.findById(id);
+            return Optional.empty();
         }
         return Optional.empty();
     }
 
     @PostMapping("/create")
     public EBikeImpl postEBike(@RequestBody EBikeImpl ebike) {
-        return this.repository.save(ebike);
+        return this.eBikeService.createNewEBike(ebike);
     }
 
     @PutMapping("{id}")
     public ResponseEntity<EBikeImpl> putEBike(final @PathVariable String id, final @RequestBody EBikeImpl ebike) {
+        return null;
+        /*
         return this.repository.existsById(id) ?
                 new ResponseEntity<EBikeImpl>(this.repository.save(ebike), HttpStatus.OK) :
-                new ResponseEntity<EBikeImpl>(this.postEBike(ebike), HttpStatus.CREATED);
+                new ResponseEntity<EBikeImpl>(this.postEBike(ebike), HttpStatus.CREATED);*/
     }
 
 }
