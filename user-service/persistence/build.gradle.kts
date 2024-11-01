69f29174a110aa123b41218fce3d05ac7b5bd6f5
S-furi <stefano.furi@studio.unibo.it>
2024-11-01 18:07:53 +0100

    feat(user): add persistency layer

---
+++ user-service/persistence/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("buildlogic.kotlin-application-conventions")
+}
+
+dependencies {
+    implementation(libs.logback)
+    implementation(libs.kotlinx.coroutine)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.bundles.test.kotest)
+    implementation(libs.bundles.exposed)
+    implementation(libs.postgres)
+}
--- user-service/settings.gradle.kts
+++ user-service/settings.gradle.kts
@@ -12,4 +12,4 @@ plugins {
 
 rootProject.name = "user-service"
 
-include("app")
+include("core", "persistence")
