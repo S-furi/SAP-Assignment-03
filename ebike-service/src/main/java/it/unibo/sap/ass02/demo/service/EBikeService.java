30bba37a2854975bea3770efb4473b5a762ea609
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-01 10:44:15 +0100

    feat: create a new service for all the ebike operations

--- ebike-service/src/main/java/it/unibo/sap/ass02/demo/service/EBikeService.java
+++ ebike-service/src/main/java/it/unibo/sap/ass02/demo/service/EBikeService.java
@@ -1,2 +1,29 @@
-package it.unibo.sap.ass02.demo.service;public class EBikeService {
+package it.unibo.sap.ass02.demo.service;
+
+import it.unibo.sap.ass02.demo.domain.EBikeImpl;
+import it.unibo.sap.ass02.demo.domain.P2d;
+import it.unibo.sap.ass02.demo.repositories.EBikeRepository;
+import it.unibo.sap.ass02.demo.repositories.P2dRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EBikeService {
+    private final EBikeRepository eBikeRepository;
+    private final P2dRepository p2dRepository;
+
+    @Autowired
+    public EBikeService(final EBikeRepository eBikeRepository, final P2dRepository p2dRepository) {
+        this.eBikeRepository = eBikeRepository;
+        this.p2dRepository = p2dRepository;
+    }
+
+    public EBikeImpl createNewEBike(EBikeImpl eBike) {
+        final P2d location = eBike.getLocation();
+        final P2d savedLocation = this.p2dRepository
+                .findByXAndY(location.x(), location.y())
+                .orElseGet(() -> this.p2dRepository.save(location));
+        eBike.updateLocation(savedLocation);
+        return this.eBikeRepository.save(eBike);
+    }
 }
