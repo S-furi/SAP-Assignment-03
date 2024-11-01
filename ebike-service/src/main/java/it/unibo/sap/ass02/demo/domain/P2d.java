cce5070c6f4a64cb08a16a758330eca8f1b8ae61
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-01 10:43:15 +0100

    feat: add inside the P2d class all the information for the persistence

--- ebike-service/src/main/java/it/unibo/sap/ass02/demo/domain/P2d.java
+++ ebike-service/src/main/java/it/unibo/sap/ass02/demo/domain/P2d.java
@@ -2,6 +2,15 @@ package it.unibo.sap.ass02.demo.domain;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
 
 /**
  *
@@ -9,9 +18,18 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * objects are completely state-less
  *
  */
-public class P2d implements java.io.Serializable {
-    private final double x;
-    private final double y;
+@Data
+@Entity
+public class P2d {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private double x = 0;
+    private double y = 0;
+
+    public P2d() {
+        this.id = 0L;
+    }
 
     public P2d(double x, double y){
         this.x=x;
