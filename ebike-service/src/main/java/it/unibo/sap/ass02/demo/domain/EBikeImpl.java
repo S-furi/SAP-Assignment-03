17dfe3f2e7fabe5680827fba56b8368d6b636920
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-07 15:57:58 +0100

    feat: add swagger documentation

--- ebike-service/src/main/java/it/unibo/sap/ass02/demo/domain/EBikeImpl.java
+++ ebike-service/src/main/java/it/unibo/sap/ass02/demo/domain/EBikeImpl.java
@@ -3,12 +3,15 @@ package it.unibo.sap.ass02.demo.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Transient;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.NoArgsConstructor;
 
 import java.util.Objects;
@@ -16,22 +19,29 @@ import java.util.Objects;
 @Entity
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Schema(description = "Ebike class, used to represent a generic ebike, this ebikes can be used for multiple purpose such as rides")
 public class EBikeImpl implements EBike<String>{
     @Id
+    @Schema(description = "Unique identifier for the ebike", example = "UU1")
     private String id;
     @JsonProperty("state")
+    @Schema(description = "Ebike state")
     private EBikeState state;
     @JsonProperty("location")
+    @Schema(description = "Global ebike's position", example = "x:0, y:0")
     @ManyToOne
     private P2d loc = null;
     @JsonIgnore()
     @Transient
     private V2d direction;
     @JsonProperty("speed")
+    @Schema(description = "Ebike's speed")
     private double speed;
     @JsonProperty("battery")
     @Min(0)
     @Max(100)
+    @Size(min = 0, max = 100)
+    @Schema(description = "Ebike's battery level")
     private int batteryLevel;  /* 0..100 */
 
     public EBikeImpl(String id) {
