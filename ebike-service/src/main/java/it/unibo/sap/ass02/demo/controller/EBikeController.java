3d2bc1f0244517e7c7d8030a07151e37590b59f0
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-07 15:45:51 +0100

    feat: add annotation for swagger apis

--- ebike-service/src/main/java/it/unibo/sap/ass02/demo/controller/EBikeController.java
+++ ebike-service/src/main/java/it/unibo/sap/ass02/demo/controller/EBikeController.java
@@ -1,5 +1,9 @@
 package it.unibo.sap.ass02.demo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import it.unibo.sap.ass02.demo.domain.EBikeImpl;
 import it.unibo.sap.ass02.demo.service.EBikeService;
 import jakarta.validation.Valid;
@@ -19,11 +23,19 @@ public class EBikeController {
         this.eBikeService = eBikeService;
     }
 
+    @Operation(summary = "Get all the ebike stored inside the service.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found all the ebikes stored.")
+    })
     @GetMapping("/all")
     public Iterable<EBikeImpl> getAllEBikes() {
         return this.eBikeService.getAllEBike();
     }
 
+    @Operation(summary = "Get a specific ebike using Its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Get the founded ebike, It can also empty because of the ID")
+    })
     @GetMapping("/{id}")
     public Optional<EBikeImpl> getEBikeById(@PathVariable String id) {
         if (Objects.nonNull(id)) {
@@ -32,12 +44,22 @@ public class EBikeController {
         return Optional.empty();
     }
 
+    @Operation(summary = "Add a new ebike into the service")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Everything went ok, all the input values were good"),
+            @ApiResponse(responseCode = "400", description = "If one of the parameters Its not correct")
+    })
     @PostMapping("/create")
     public EBikeImpl postEBike(@Valid @RequestBody EBikeImpl ebike) {
         return this.eBikeService.createNewEBike(ebike);
     }
 
-    @PutMapping("/{id}")
+    @Operation(summary = "Update an existing ebike by using Its ID, if It does not exist we try to create a new ebike.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Everything went ok, all the input values were good"),
+            @ApiResponse(responseCode = "400", description = "If one of the parameters Its not correct")
+    })
+    @PutMapping("/update/{id}")
     public ResponseEntity<EBikeImpl> putEBike(final @PathVariable String id, final @RequestBody EBikeImpl ebike) {
         return this.eBikeService.existsById(id) ?
                 new ResponseEntity<EBikeImpl>(this.eBikeService.createNewEBike(ebike), HttpStatus.OK) :
