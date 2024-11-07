0fad0e2e5982d68b2840dffe4122ec8ec9aa6b80
S-furi <stefano.furi@studio.unibo.it>
2024-11-07 09:51:55 +0100

    chore: make use of dumb db in tests

--- user-service/persistence/src/test/kotlin/it/unibo/sap/ass02/persistence/UserDatabaseTest.kt
+++ user-service/persistence/src/test/kotlin/it/unibo/sap/ass02/persistence/UserDatabaseTest.kt
@@ -3,20 +3,12 @@ package it.unibo.sap.ass02.persistence
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 import it.unibo.sap.ass02.domain.UserImpl
-import it.unibo.sap.ass02.persistence.utils.DatabaseUtils
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.transaction
+import it.unibo.sap.ass02.persistence.utls.DumbUserDB
 
 class UserDatabaseTest : FunSpec({
-    afterEach {
-        transaction(DatabaseUtils.database) {
-            SchemaUtils.drop(UserRepository.Users)
-        }
-    }
-
 
     test("Some operations should work") {
-        val userRepository = UserRepository(DatabaseUtils.database)
+        val userRepository = DumbUserDB()
         val u = UserImpl(1)
         userRepository.create(u) shouldBe 1
         userRepository.findById(1) shouldBe u
