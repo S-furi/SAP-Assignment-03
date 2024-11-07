9ae220d12d0aa78b92d45a93babce1e6d09dcea3
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-07 18:11:13 +0100

    test: add getAll ebikes and put Ebike test

--- ebike-service/src/test/java/it/unibo/sap/ass02/demo/EbikeServiceApplicationTests.java
+++ ebike-service/src/test/java/it/unibo/sap/ass02/demo/EbikeServiceApplicationTests.java
@@ -1,13 +1,81 @@
 package it.unibo.sap.ass02.demo;
 
+import it.unibo.sap.ass02.demo.domain.EBikeImpl;
+import it.unibo.sap.ass02.demo.repositories.EBikeRepository;
+import jakarta.annotation.PostConstruct;
+import org.json.JSONArray;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-@SpringBootTest
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class EbikeServiceApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	private String address = "";
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@PostConstruct
+	private void buildAddress() {
+		this.address = "http://localhost:" + this.port + "/ebike";
+	}
 
 	@Test
 	void contextLoads() {
+
+	}
+
+	@Test
+	void postNewEBikeTest() {
+		final EBikeImpl ebike = new EBikeImpl("UU3");
+		assertEquals(this.restTemplate.postForEntity(this.address + "/create", ebike, EBikeImpl.class).getStatusCode(), HttpStatus.OK);
+	}
+
+	@Test
+	void getAllEBikes() {
+		final List<EBikeImpl> ebikes = List.of(
+				new EBikeImpl(UUID.randomUUID().toString()),
+				new EBikeImpl(UUID.randomUUID().toString()));
+		ebikes.forEach(ebike -> {
+			this.restTemplate.postForEntity(this.address + "/create", ebike, EBikeImpl.class);
+		});
+		final ResponseEntity<List<EBikeImpl>> response = this.restTemplate.exchange(
+				this.address + "/all",
+				HttpMethod.GET,
+				null,
+				new ParameterizedTypeReference<List<EBikeImpl>>() {}
+		);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertTrue(!Objects.requireNonNull(response.getBody()).isEmpty());
+		ebikes.forEach(ebike -> {
+			assertTrue(response.getBody().contains(ebike));
+		});
 	}
 
 }
