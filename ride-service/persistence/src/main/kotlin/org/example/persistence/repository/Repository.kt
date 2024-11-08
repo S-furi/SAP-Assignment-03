e1175217c6bcbaf6399c744f9d7212533f5d662c
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-08 17:01:17 +0100

    feat(async): add suspend keyword to all the functions

--- ride-service/persistence/src/main/kotlin/org/example/persistence/repository/Repository.kt
+++ ride-service/persistence/src/main/kotlin/org/example/persistence/repository/Repository.kt
@@ -2,8 +2,8 @@ package org.example.persistence.repository
 import org.example.app.Entity
 
 interface Repository<K : Any, E : Entity<K>> {
-    fun save(entity: E) : Int?;
-    fun update(entity: E) : Int;
-    fun findAll() : Iterable<E>
-    fun findByID(id : K) : E?
+    suspend fun save(entity: E) : Int?;
+    suspend fun update(entity: E) : Int;
+    suspend fun findAll() : Iterable<E>
+    suspend fun findByID(id : K) : E?
 }
