7a9c9e62a31a7407a507c00ce21ee6912c216d89
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-08 16:46:12 +0100

    feat(utils): add utils for db connection

---
+++ ride-service/persistence/src/main/kotlin/org/example/persistence/repository/utils/DatabaseUtils.kt
@@ -0,0 +1,22 @@
+package org.example.persistence.repository.utils
+
+import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
+
+object DatabaseUtils {
+    private const val DB_NAME = "ANAME";
+    private const val DB_USER = "AUSER";
+    private const val DB_PASSWORD = "APASSWORD";
+
+    val database = Database.connect(
+        url = "jdbc:postgresql://${System.getenv("POSTGRES_HOST") ?: "localhost"}:5432/$DB_NAME",
+        user = DB_USER,
+        password = DB_PASSWORD
+    )
+
+    suspend fun <T> executeQuery(block: suspend() -> T) : T {
+        return newSuspendedTransaction(Dispatchers.IO) { block() }
+    }
+
+}
