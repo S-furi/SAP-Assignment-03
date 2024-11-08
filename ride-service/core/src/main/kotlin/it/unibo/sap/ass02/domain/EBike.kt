98c6a67a1933a66c1f07c18751ab6023a84a153b
S-furi <stefano.furi@studio.unibo.it>
2024-11-08 17:10:16 +0100

    feat(ride): initialize core interfaces

---
+++ ride-service/core/src/main/kotlin/it/unibo/sap/ass02/domain/EBike.kt
@@ -0,0 +1,5 @@
+package it.unibo.sap.ass02.domain
+
+interface EBike {
+
+}
---
+++ ride-service/core/src/main/kotlin/it/unibo/sap/ass02/domain/Ride.kt
@@ -0,0 +1,11 @@
+package it.unibo.sap.ass02.domain
+
+import it.unibo.sap.ass02.domain.ddd.Entity
+import java.util.Date
+
+interface Ride : Entity<Int>{
+    val ebike: EBike
+    val user: User
+    val startedDate: Date
+    val endDate: Date
+}
