7778bcc77315956ff9c7258e73ca5148fcf070f6
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-08 17:01:36 +0100

    feat: add partial implementation of the save query

--- ride-service/persistence/src/main/kotlin/org/example/persistence/repository/RideRepository.kt
+++ ride-service/persistence/src/main/kotlin/org/example/persistence/repository/RideRepository.kt
@@ -1,14 +1,20 @@
 package org.example.persistence.repository
 
 
+import kotlinx.serialization.json.internal.decodeByReader
 import org.example.app.Ride
 import org.example.persistence.repository.utils.DatabaseUtils
+import org.example.persistence.repository.utils.DatabaseUtils.dbQuery
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.javatime.date
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.LocalDate
 
 class RideRepository(database: Database = DatabaseUtils.database): DatabaseRepository<Int, Ride>(database){
-    object Ride: Table() {
+    object Rides: Table() {
         val id = integer("id")
         val ebike = varchar("ebike_id", length = 100)
         val user = integer("user_id")
@@ -17,19 +23,35 @@ class RideRepository(database: Database = DatabaseUtils.database): DatabaseRepos
         override val primaryKey = PrimaryKey(id)
     }
 
-    override fun save(entity: org.example.app.Ride): Int? {
+    init {
+        transaction(this.database){
+            SchemaUtils.create(Rides)
+        }
+    }
+
+    override suspend fun save(entity: Ride): Int? = dbQuery {
+        if (this.findByID(entity.getId()) == null) {
+            Rides.insert {
+                it[id] = entity.getId()
+                it[ebike] = ""
+                it[user] = 0
+                it[startDate] = LocalDate.now()
+                it[endingDate] = LocalDate.now()
+            }[Rides.id]
+        }else{
+            null
+        }
+    }
+
+    override suspend fun update(entity: Ride): Int {
         TODO("Not yet implemented")
     }
 
-    override fun update(entity: org.example.app.Ride): Int {
+    override suspend fun findAll(): Iterable<Ride> {
         TODO("Not yet implemented")
     }
 
-    override fun findAll(): Iterable<org.example.app.Ride> {
-        TODO("Not yet implemented")
-    }
-
-    override fun findByID(id: Int): org.example.app.Ride? {
+    override suspend fun findByID(id: Int): Ride? {
         TODO("Not yet implemented")
     }
 }
