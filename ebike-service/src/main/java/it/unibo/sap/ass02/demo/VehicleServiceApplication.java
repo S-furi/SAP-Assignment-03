c8ebefeb8b81e8c53ccb29616c196c8a4d8f98ab
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-08 10:01:18 +0100

    refactor: rename classes

--- ebike-service/src/main/java/it/unibo/sap/ass02/demo/VehicleServiceApplication.java
+++ ebike-service/src/main/java/it/unibo/sap/ass02/demo/VehicleServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EbikeServiceApplication {
+public class VehicleServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EbikeServiceApplication.class, args);
+		SpringApplication.run(VehicleServiceApplication.class, args);
 	}
 
 }
--- ebike-service/src/test/java/it/unibo/sap/ass02/demo/VehicleServiceApplicationTests.java
+++ ebike-service/src/test/java/it/unibo/sap/ass02/demo/VehicleServiceApplicationTests.java
@@ -31,7 +31,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-class EbikeServiceApplicationTests {
+class VehicleServiceApplicationTests {
 
 	@LocalServerPort
 	private int port;
