34aed495f74cc6488dfe21a7590c4927ac28f551
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-09 12:25:13 +0100

    refactor: change the ebike id type, Int -> String

--- ride-service/core/src/main/kotlin/it/unibo/sap/ass02/domain/model/EBike.kt
+++ ride-service/core/src/main/kotlin/it/unibo/sap/ass02/domain/model/EBike.kt
@@ -2,7 +2,7 @@ package it.unibo.sap.ass02.domain.model
 
 import it.unibo.sap.ass02.domain.ddd.Entity
 
-interface EBike : Entity<Int> {
+interface EBike : Entity<String> {
     val position: P2d
     val direction: V2d
     val speed: Double
