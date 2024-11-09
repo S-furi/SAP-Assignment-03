746a49a2d3545668d5b3e43b937888acbf856359
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-09 12:26:20 +0100

    refactor: change date type, from Date to LocalDate

--- ride-service/core/src/main/kotlin/it/unibo/sap/ass02/domain/Ride.kt
+++ ride-service/core/src/main/kotlin/it/unibo/sap/ass02/domain/Ride.kt
@@ -3,13 +3,14 @@ package it.unibo.sap.ass02.domain
 import it.unibo.sap.ass02.domain.ddd.Entity
 import it.unibo.sap.ass02.domain.model.EBike
 import it.unibo.sap.ass02.domain.model.User
+import java.time.LocalDate
 import java.util.Date
 
 interface Ride : Entity<Int> {
     val ebike: EBike
     val user: User
-    val startedDate: Date
-    val endDate: Date
+    val startedDate: LocalDate
+    val endDate: LocalDate
 
     fun end()
 }
