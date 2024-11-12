ff17075acb1c05a45fdf894c0555a2cbce7d8c92
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-12 16:03:29 +0100

    feat: add functions for DB interaction

--- ride-service/service/src/main/kotlin/it/unibo/sap/ass02/service/api/RideResolver.kt
+++ ride-service/service/src/main/kotlin/it/unibo/sap/ass02/service/api/RideResolver.kt
@@ -6,5 +6,9 @@ import it.unibo.sap.ass02.persistence.repository.RideRepository
 object  RideResolver {
     private val repository = RideRepository()
     suspend fun getAllRides(): Iterable<Ride> = this.repository.findAll()
+    suspend fun getRideByID(id: Int) : Ride? = this.repository.findByID(id)
+    suspend fun addNewRide(ride: Ride): Int? = this.repository.save(ride)
+    suspend fun updateRide(ride: Ride): Int = this.repository.update(ride)
+    suspend fun deleteRide(id: Int): Unit = print("TODO")
 
 }
