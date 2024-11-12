7f43dad89d4c1246c5cd9696e04bacf8506ac881
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-12 17:28:32 +0100

    refactor: general refactor, refactor main function to handle clients request. I am very proud of that i want the all the

--- ride-service/service/src/main/kotlin/it/unibo/sap/ass02/service/api/RideRouting.kt
+++ ride-service/service/src/main/kotlin/it/unibo/sap/ass02/service/api/RideRouting.kt
@@ -4,7 +4,10 @@ import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import it.unibo.sap.ass02.service.api.RideRoutes.ALL_RIDES
+import it.unibo.sap.ass02.service.api.RideRoutes.CREATE_RIDE
+import it.unibo.sap.ass02.service.api.RideRoutes.DELETE_RIDE
 import it.unibo.sap.ass02.service.api.RideRoutes.RIDE_BY_ID
+import it.unibo.sap.ass02.service.api.RideRoutes.UPDATE_RIDE
 
 object RideRouting {
     fun Route.rideRouting() {
@@ -13,9 +16,33 @@ object RideRouting {
         }
 
         get(RIDE_BY_ID) {
-            handleGetRideByID(call)
+//            handleGetRideByID(call)
+            handleClientRequest(
+                parameter = {call.parameters["id"]?.toInt()},
+                handler = {id -> RideResolver.getRideByID(id) },
+                handleOK = {obj -> call.respond(HttpStatusCode.OK, obj)},
+                errorQuery = { call.respond(HttpStatusCode.BadRequest, "The input ID does not exist.") },
+                errorNotFound = {call.respond(HttpStatusCode.BadRequest, "The input parameter is null.")})
+        }
+
+        post(CREATE_RIDE) {
+
+        }
+
+        put(UPDATE_RIDE) {
+
+        }
+
+        delete(DELETE_RIDE) {
+            handleClientRequest(
+                parameter = { call.parameters["id"]?.toInt() },
+                handler = {id -> RideResolver.deleteRide(id) },
+                handleOK = {obj -> call.respond(HttpStatusCode.OK, obj)},
+                errorQuery = { call.respond(HttpStatusCode.BadRequest, "The input ID does not exist.") },
+                errorNotFound = {call.respond(HttpStatusCode.BadRequest, "The input parameter is null")})
         }
     }
+/*
 
     private suspend fun handleGetRideByID(call: RoutingCall) {
         val rideID = call.parameters["id"]?.toInt()
@@ -28,5 +55,18 @@ object RideRouting {
                 call.respond(HttpStatusCode.OK, it)
             } ?: call.respond(HttpStatusCode.NotFound, message)
         }
+    }
+*/
+
+    private suspend fun <K, T>handleClientRequest(parameter: () -> K?,
+                                                  handler: suspend (K) -> T?,
+                                                  handleOK: suspend (T) -> Unit,
+                                                  errorQuery: suspend () -> Unit,
+                                                  errorNotFound: suspend () -> Unit) {
+        parameter()?.let {p ->
+            handler(p)?.let {obj ->
+                handleOK(obj)
+            }?:errorQuery()
+        }?:errorNotFound()
     }
 }
