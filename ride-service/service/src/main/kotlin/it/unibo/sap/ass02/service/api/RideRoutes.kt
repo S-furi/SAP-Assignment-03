cc302b012a662fc099ddf9c0d8716c7626d84f32
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-12 16:40:53 +0100

    feat(routes): add all the main routes

---
+++ ride-service/service/src/main/kotlin/it/unibo/sap/ass02/service/api/RideRoutes.kt
@@ -0,0 +1,10 @@
+package it.unibo.sap.ass02.service.api
+
+object RideRoutes {
+    private const val BASE_PATH = "/api/rides"
+    const val ALL_RIDES = "$BASE_PATH/all"
+    const val RIDE_BY_ID = "$BASE_PATH/find/{id}"
+    const val CREATE_RIDE = "$BASE_PATH/create"
+    const val UPDATE_RIDE = "$BASE_PATH/update/{id}"
+    const val DELETE_RIDE = "$BASE_PATH/delete/{id}"
+}
