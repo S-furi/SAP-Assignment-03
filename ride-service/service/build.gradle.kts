1eea30ba7f4052e3678d51b7ab14f8ef0f91a2b7
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-12 17:43:39 +0100

    feat: add kotlinx lib

--- ride-service/service/build.gradle.kts
+++ ride-service/service/build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
 dependencies {
     implementation("io.ktor:ktor-server-core:3.0.1")
     implementation("io.ktor:ktor-server-netty:3.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 }
