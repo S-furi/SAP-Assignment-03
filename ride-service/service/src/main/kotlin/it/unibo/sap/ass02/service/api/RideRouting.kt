ae6819b12fb075e5b7afed3770a74e997cfd4068
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-12 16:58:04 +0100

    feat: add implementation for get RIDE_BY_ID using the Elvis Operator

--- ride-service/service/src/main/kotlin/it/unibo/sap/ass02/service/api/RideRouting.kt
+++ ride-service/service/src/main/kotlin/it/unibo/sap/ass02/service/api/RideRouting.kt
@@ -1,13 +1,32 @@
 package it.unibo.sap.ass02.service.api
 
+import io.ktor.http.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import it.unibo.sap.ass02.service.api.RideRoutes.ALL_RIDES
+import it.unibo.sap.ass02.service.api.RideRoutes.RIDE_BY_ID
 
 object RideRouting {
     fun Route.rideRouting() {
         get(ALL_RIDES) {
             call.respond(RideResolver.getAllRides())
+        }
+
+        get(RIDE_BY_ID) {
+            handleGetRideByID(call)
+        }
+    }
+
+    private suspend fun handleGetRideByID(call: RoutingCall) {
+        val rideID = call.parameters["id"]?.toInt()
+        val message = "Error: the input Ride id does not exist or It is null."
+        if (rideID == null) {
+            call.respond(HttpStatusCode.BadRequest, message)
+            return
+        }else {
+            RideResolver.getRideByID(rideID)?.let {
+                call.respond(HttpStatusCode.OK, it)
+            } ?: call.respond(HttpStatusCode.NotFound, message)
         }
     }
 }
