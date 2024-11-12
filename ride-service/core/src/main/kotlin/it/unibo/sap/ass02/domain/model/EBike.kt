69370133ab969015d54a222e5230d9c0a90dfbb7
S-furi <stefano.furi@studio.unibo.it>
2024-11-12 16:00:03 +0100

    feat(ride): make bike logic use stub

--- ride-service/core/src/main/kotlin/it/unibo/sap/ass02/domain/model/EBike.kt
+++ ride-service/core/src/main/kotlin/it/unibo/sap/ass02/domain/model/EBike.kt
@@ -3,9 +3,12 @@ package it.unibo.sap.ass02.domain.model
 import it.unibo.sap.ass02.domain.ddd.Entity
 
 interface EBike : Entity<String> {
-    val location: P2d
-    val direction: V2d
-    val speed: Double
+    var location: P2d
+    var direction: V2d
+    var speed: Double
+    var state: String // TODO: create an enum
+    var available: Boolean
+    var battery: Int
 
     fun updateSpeed(speed: Double)
 
--- ride-service/core/src/main/kotlin/it/unibo/sap/ass02/domain/model/stub/EBikeImpl.kt
+++ ride-service/core/src/main/kotlin/it/unibo/sap/ass02/domain/model/stub/EBikeImpl.kt
@@ -4,25 +4,58 @@ import it.unibo.sap.ass02.domain.model.EBike
 import it.unibo.sap.ass02.domain.model.P2d
 import it.unibo.sap.ass02.domain.model.V2d
 
+/**
+ * TODO: implement here circuit breaker in case BikeAPI doesn't work and no results are yielded (do not use`!!`)
+ */
 class EBikeImpl(
     override val id: String,
 ) : EBike {
-    override val location: P2d
-        get() = TODO("Not yet implemented")
-    override val direction: V2d
-        get() = TODO("Not yet implemented")
-    override val speed: Double
-        get() = TODO("Not yet implemented")
+    override var location: P2d = EBikeStub.location(this.id)!!
+        get() = EBikeStub.location(this.id)!!
+    override var direction: V2d = EBikeStub.direction(this.id)!!
+        get() = EBikeStub.direction(this.id)!!
+    override var speed: Double = EBikeStub.speed(this.id)!!
+        get() = EBikeStub.speed(this.id)!!
+    override var state: String = EBikeStub.state(this.id)!!
+        get() = EBikeStub.state(this.id)!!
+    override var available: Boolean = EBikeStub.isAvailable(this.id)!!
+        get() = EBikeStub.isAvailable(this.id)!!
+    override var battery: Int = EBikeStub.battery(this.id)!!
+        get() = EBikeStub.battery(this.id)!!
 
     override fun updateSpeed(speed: Double) {
-        TODO("Not yet implemented")
+        if (EBikeStub.updateBike(this.copy(speed = speed))) {
+            this.speed = speed
+        }
     }
 
     override fun updateLocation(pos: P2d) {
-        TODO("Not yet implemented")
+        if (EBikeStub.updateBike(this.copy(location = pos))) {
+            this.location = pos
+        }
     }
 
     override fun updateDirection(dir: V2d) {
-        TODO("Not yet implemented")
+        if (EBikeStub.updateBike(this.copy(direction = dir))) {
+            this.direction = dir
+        }
+    }
+
+    private fun copy(
+        location: P2d = this.location,
+        direction: V2d = this.direction,
+        speed: Double = this.speed,
+        state: String = this.state,
+        available: Boolean = this.available,
+        battery: Int = this.battery,
+    ): EBike {
+        val b = EBikeImpl(this.id)
+        b.location = location
+        b.direction = direction
+        b.speed = speed
+        b.state = state
+        b.available = available
+        b.battery = battery
+        return b
     }
 }
