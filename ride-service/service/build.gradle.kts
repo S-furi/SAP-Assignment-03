2059d75569bc937806b8485073f84a07e727414b
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-12 15:50:25 +0100

    feat: add ktor dependencies

--- ride-service/service/build.gradle.kts
+++ ride-service/service/build.gradle.kts
@@ -1,3 +1,8 @@
 plugins {
     id("buildlogic.kotlin-application-conventions")
 }
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:3.0.1")
+    implementation("io.ktor:ktor-server-netty:3.0.1")
+}
