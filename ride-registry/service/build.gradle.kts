554b14cd6e8ac11a190290d4d10561a052f73bc8
S-furi <stefano.furi@studio.unibo.it>
2024-11-19 11:31:18 +0100

    refactor(ride): rename package

---
+++ ride-registry/service/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("buildlogic.kotlin-application-conventions")
+}
+
+dependencies {
+    implementation(libs.bundles.ktor.server)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.bundles.exposed)
+    implementation(project(":core"))
+    implementation(project(":persistence"))
+}
