8df9f6cbc59fe24076c7bd3d55695de97fe759d5
S-furi <stefano.furi@studio.unibo.it>
2024-11-20 10:56:42 +0100

    refactor: cleanup code

--- ride-registry/persistence/src/main/kotlin/it/unibo/sap/ass02/persistence/repository/utils/DatabaseUtils.kt
+++ ride-registry/persistence/src/main/kotlin/it/unibo/sap/ass02/persistence/repository/utils/DatabaseUtils.kt
@@ -5,18 +5,16 @@ import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 
 object DatabaseUtils {
-    private const val DB_NAME = "ANAME";
-    private const val DB_USER = "AUSER";
-    private const val DB_PASSWORD = "APASSWORD";
+    private const val DB_NAME = "ANAME"
+    private const val DB_USER = "AUSER"
+    private const val DB_PASSWORD = "APASSWORD"
 
-    val database = Database.connect(
-        url = "jdbc:postgresql://${System.getenv("POSTGRES_HOST") ?: "localhost"}:5432/$DB_NAME",
-        user = DB_USER,
-        password = DB_PASSWORD
-    )
+    val database =
+        Database.connect(
+            url = "jdbc:postgresql://${System.getenv("POSTGRES_HOST") ?: "localhost"}:5432/$DB_NAME",
+            user = DB_USER,
+            password = DB_PASSWORD,
+        )
 
-    suspend fun <T> dbQuery(block: suspend() -> T) : T {
-        return newSuspendedTransaction(Dispatchers.IO) { block() }
-    }
-
+    suspend fun <T> dbQuery(block: suspend () -> T): T = newSuspendedTransaction(Dispatchers.IO) { block() }
 }
