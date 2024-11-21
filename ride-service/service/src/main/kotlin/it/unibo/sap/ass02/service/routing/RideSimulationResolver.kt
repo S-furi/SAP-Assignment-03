a93b606970e81968119e35257bea9afc0e9e2f76
S-furi <stefano.furi@studio.unibo.it>
2024-11-21 15:13:39 +0100

    feat(ride-service): make use of websocket only kind of service

--- ride-service/service/src/main/kotlin/it/unibo/sap/ass02/service/routing/RideSimulationResolver.kt
+++ ride-service/service/src/main/kotlin/it/unibo/sap/ass02/service/routing/RideSimulationResolver.kt
@@ -1,40 +1,13 @@
 package it.unibo.sap.ass02.service.routing
 
 import it.unibo.sap.ass02.domain.Ride
-import it.unibo.sap.ass02.domain.RideImpl
-import it.unibo.sap.ass02.domain.model.EBike
-import it.unibo.sap.ass02.infrastructure.stub.impl.EBikeImpl
-import it.unibo.sap.ass02.domain.model.User
-import it.unibo.sap.ass02.infrastructure.stub.impl.UserImpl
 
 object RideSimulationResolver {
     private val registry = mutableMapOf<Int, Ride>()
-    private var id = 0
 
-    suspend fun createRide(
-        bikeId: String,
-        userId: Int,
-    ): Int? =
-        createActualRide(
-            EBikeImpl(bikeId),
-            UserImpl(userId),
-        )?.id
+    fun findRide(id: Int) = registry[id]
 
-    private suspend fun createActualRide(
-        bike: EBike,
-        user: User,
-    ): Ride? =
-        runCatching {
-            val ebike = RideImpl(bike, user, id = ++id)
-            this.registry[ebike.id] = ebike
-            ebike
-        }.getOrNull()
+    fun startRide(id: Int) = registry[id]?.also { it.start() }
 
-    suspend fun deleteRide(id: Int) = registry.remove(id)?.id
-
-    suspend fun findRide(id: Int) = registry[id]
-
-    suspend fun startRide(id: Int) = registry[id]?.also { it.start() }
-
-    suspend fun stopRide(id: Int) = registry[id]?.also { it.end() }
+    fun stopRide(id: Int) = registry[id]?.also { it.end() }
 }
