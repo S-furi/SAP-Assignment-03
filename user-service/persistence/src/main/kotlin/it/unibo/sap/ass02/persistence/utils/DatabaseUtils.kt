c8067347ee00c776452405786d19b200daf5c503
S-furi <stefano.furi@studio.unibo.it>
2024-11-21 18:42:28 +0100

    build(user): fix postgres naming

--- user-service/persistence/src/main/kotlin/it/unibo/sap/ass02/persistence/utils/DatabaseUtils.kt
+++ user-service/persistence/src/main/kotlin/it/unibo/sap/ass02/persistence/utils/DatabaseUtils.kt
@@ -6,8 +6,8 @@ import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransacti
 
 object DatabaseUtils {
     private const val DB_NAME = "user-service"
-    private const val USER = "roott"
-    private const val PASSWORD = "test123"
+    private const val USER = "postgres"
+    private const val PASSWORD = "postgres"
 
     val database = Database.connect(
         url = "jdbc:postgresql://${System.getenv("POSTGRES_HOST") ?: "localhost"}:5432/$DB_NAME",
