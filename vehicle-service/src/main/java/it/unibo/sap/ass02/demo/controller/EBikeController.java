f20b3a13fba2964c7622b8f1c13980394863d92b
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-22 15:22:03 +0100

    feat: replace hardcoded routes with static variables

--- vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/EBikeController.java
+++ vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/EBikeController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import it.unibo.sap.ass02.demo.domain.EBikeImpl;
 import it.unibo.sap.ass02.demo.service.EBikeService;
+import it.unibo.sap.ass02.demo.service.routes.EBikeRoutes;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
@@ -14,7 +15,7 @@ import java.util.Objects;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/ebike")
+@RequestMapping(EBikeRoutes.EBIKE_ROUTE)
 public class EBikeController {
     @NotNull
     private final EBikeService eBikeService;
@@ -26,7 +27,7 @@ public class EBikeController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Found all the ebikes stored.")
     })
-    @GetMapping("/all")
+    @GetMapping(EBikeRoutes.ALL_EBIKES)
     public Iterable<EBikeImpl> getAllEBikes() {
         return this.eBikeService.getAllEBike();
     }
@@ -35,7 +36,7 @@ public class EBikeController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Get the founded ebike, It can also empty because of the ID")
     })
-    @GetMapping("/{id}")
+    @GetMapping(EBikeRoutes.SINGLE_EBIKE)
     public Optional<EBikeImpl> getEBikeById(@PathVariable String id) {
         if (Objects.nonNull(id)) {
             return this.eBikeService.getEBikeById(id);
@@ -48,7 +49,7 @@ public class EBikeController {
             @ApiResponse(responseCode = "201", description = "Everything went ok, all the input values were good"),
             @ApiResponse(responseCode = "400", description = "If one of the parameters Its not correct")
     })
-    @PostMapping("/create")
+    @PostMapping(EBikeRoutes.CREATE_EBIKE)
     public ResponseEntity<EBikeImpl> postEBike(@Valid @RequestBody EBikeImpl ebike) {
         return new ResponseEntity<>(this.eBikeService.createNewEBike(ebike), HttpStatus.CREATED);
     }
@@ -58,14 +59,14 @@ public class EBikeController {
             @ApiResponse(responseCode = "200", description = "Everything went ok, all the input values were good"),
             @ApiResponse(responseCode = "400", description = "If one of the parameters Its not correct")
     })
-    @PutMapping("/update/{id}")
+    @PutMapping(EBikeRoutes.UPDATE_EBIKE)
     public ResponseEntity<EBikeImpl> putEBike(final @PathVariable String id, final @RequestBody EBikeImpl ebike) {
         return this.eBikeService.existsById(id) ?
                 new ResponseEntity<EBikeImpl>(this.eBikeService.createNewEBike(ebike), HttpStatus.OK) :
                 this.postEBike(ebike);
     }
 
-    @PutMapping("/update/location/{id}")
+    @PutMapping(EBikeRoutes.UPDATE_EBIKE_LOCATION)
     @Operation(summary = "Update an ebike location using the input parameters.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Everything went ok, all the input values were good"),
@@ -79,7 +80,7 @@ public class EBikeController {
     }
 
 
-    @PutMapping("/update/battery/{id}")
+    @PutMapping(EBikeRoutes.UPDATE_EBIKE_BATTERY)
     @Operation(summary = "Update ebike's battery level using the input parameters.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Everything went ok, all the input values were good"),
