95dc10df4e167681c39f3da2ea442d392a43acf8
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-22 15:13:07 +0100

    feat(api): add new endpoint for updating the ebike battery level

--- vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/EBikeController.java
+++ vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/EBikeController.java
@@ -3,7 +3,6 @@ package it.unibo.sap.ass02.demo.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import io.swagger.v3.oas.models.annotations.OpenAPI30;
 import it.unibo.sap.ass02.demo.domain.EBikeImpl;
 import it.unibo.sap.ass02.demo.service.EBikeService;
 import jakarta.validation.Valid;
@@ -50,8 +49,8 @@ public class EBikeController {
             @ApiResponse(responseCode = "400", description = "If one of the parameters Its not correct")
     })
     @PostMapping("/create")
-    public EBikeImpl postEBike(@Valid @RequestBody EBikeImpl ebike) {
-        return this.eBikeService.createNewEBike(ebike);
+    public ResponseEntity<EBikeImpl> postEBike(@Valid @RequestBody EBikeImpl ebike) {
+        return new ResponseEntity<>(this.eBikeService.createNewEBike(ebike), HttpStatus.CREATED);
     }
 
     @Operation(summary = "Update an existing ebike by using Its ID, if It does not exist we try to create a new ebike.")
@@ -63,7 +62,7 @@ public class EBikeController {
     public ResponseEntity<EBikeImpl> putEBike(final @PathVariable String id, final @RequestBody EBikeImpl ebike) {
         return this.eBikeService.existsById(id) ?
                 new ResponseEntity<EBikeImpl>(this.eBikeService.createNewEBike(ebike), HttpStatus.OK) :
-                new ResponseEntity<EBikeImpl>(this.postEBike(ebike), HttpStatus.CREATED);
+                this.postEBike(ebike);
     }
 
     @PutMapping("/update/location/{id}")
@@ -77,5 +76,18 @@ public class EBikeController {
                                                     final @RequestParam Double y) {
         this.eBikeService.updateLocation(id, x, y);
         return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+
+    @PutMapping("/update/battery/{id}")
+    @Operation(summary = "Update ebike's battery level using the input parameters.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Everything went ok, all the input values were good"),
+            @ApiResponse(responseCode = "400", description = "If one of the parameters Its not correct")
+    })
+    public ResponseEntity<Void> updateBattery(final @PathVariable String id,
+                                              final @RequestParam Integer delta) {
+        this.eBikeService.updateBattery(id, delta);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
