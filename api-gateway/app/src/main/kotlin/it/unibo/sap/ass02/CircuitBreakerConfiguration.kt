3476a85110c84617d5f498c8e051d3915ea645e2
S-furi <stefano.furi@studio.unibo.it>
2024-11-23 18:15:58 +0100

    feat(api-gateway): increase CircuitBreaker parameters

--- api-gateway/app/src/main/kotlin/it/unibo/sap/ass02/CircuitBreakerConfiguration.kt
+++ api-gateway/app/src/main/kotlin/it/unibo/sap/ass02/CircuitBreakerConfiguration.kt
@@ -10,12 +10,12 @@ object CircuitBreakerConfiguration {
         CircuitBreakerConfig
             .custom()
             .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
-            .slidingWindowSize(10)
+            .slidingWindowSize(100)
             .slowCallRateThreshold(70.0f)
             .failureRateThreshold(70.0f)
             .waitDurationInOpenState(Duration.ofSeconds(5))
             .slowCallDurationThreshold(Duration.ofNanos(3))
-            .permittedNumberOfCallsInHalfOpenState(3)
+            .permittedNumberOfCallsInHalfOpenState(10)
             .build()
 
     val circuitBreaker = CircuitBreaker.of("API-Gateway", this.getConfiguration())
