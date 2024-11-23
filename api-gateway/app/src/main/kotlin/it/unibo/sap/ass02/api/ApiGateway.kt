c53e1a6e05d1bc4777a1d10346c8018cdaabcdb6
S-furi <stefano.furi@studio.unibo.it>
2024-11-23 14:34:14 +0100

    fix(api-gateway): use ride-repository for get/post operations

--- api-gateway/app/src/main/kotlin/it/unibo/sap/ass02/api/ApiGateway.kt
+++ api-gateway/app/src/main/kotlin/it/unibo/sap/ass02/api/ApiGateway.kt
@@ -7,7 +7,7 @@ import it.unibo.sap.ass02.api.ApiRoutes.USER_ROUTES
 import it.unibo.sap.ass02.api.ApiRoutes.VEHICLE_ROUTES
 import it.unibo.sap.ass02.api.RoutingCallExtensions.callWithCircuitBreaker
 import it.unibo.sap.ass02.api.RoutingCallExtensions.handleBasicGet
-import it.unibo.sap.ass02.api.ServicesURIs.RIDE_SERVICE
+import it.unibo.sap.ass02.api.ServicesURIs.RIDE_REGISTRY
 import it.unibo.sap.ass02.api.ServicesURIs.USER_SERVICE
 import it.unibo.sap.ass02.api.ServicesURIs.VEHICLE_SERVICE
 
@@ -20,7 +20,7 @@ object ApiGateway {
             call.handleBasicGet(USER_SERVICE, "users")
         }
         get(RIDE_ROUTES) {
-            call.handleBasicGet(RIDE_SERVICE)
+            call.handleBasicGet(RIDE_REGISTRY)
         }
         post(VEHICLE_ROUTES) {
             call.callWithCircuitBreaker(VEHICLE_SERVICE + VEHICLE_ROUTES, HttpMethod.Post)
@@ -29,7 +29,7 @@ object ApiGateway {
             call.callWithCircuitBreaker(USER_SERVICE + USER_ROUTES, HttpMethod.Post)
         }
         post(RIDE_ROUTES) {
-            call.callWithCircuitBreaker(RIDE_SERVICE + RIDE_ROUTES, HttpMethod.Post)
+            call.callWithCircuitBreaker(RIDE_REGISTRY + RIDE_ROUTES, HttpMethod.Post)
         }
     }
 }
