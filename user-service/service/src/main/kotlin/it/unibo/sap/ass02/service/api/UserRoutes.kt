8954d81d0538336f17582a18d9a512773a78b2f2
S-furi <stefano.furi@studio.unibo.it>
2024-11-23 11:30:26 +0100

    chore(user-service): change API path

--- user-service/service/src/main/kotlin/it/unibo/sap/ass02/service/api/UserRoutes.kt
+++ user-service/service/src/main/kotlin/it/unibo/sap/ass02/service/api/UserRoutes.kt
@@ -1,12 +1,10 @@
-@file:Suppress("ktlint:standard:filename")
-
 package it.unibo.sap.ass02.service.api
 
 object UsersRoutes {
-    private const val BASE_PATH: String = "api/users"
+    private const val BASE_PATH: String = "users"
     const val HEALTHCHECK: String = "$BASE_PATH/health"
     const val USERS: String = "$BASE_PATH/all"
-    const val USER_BY_ID: String = "$BASE_PATH/find/{id}"
+    const val USER_BY_ID: String = "$BASE_PATH/{id}"
     const val USER_ADD: String = "$BASE_PATH/new"
     const val USER_DELETE: String = "$BASE_PATH/{id}"
     const val USER_INCREASE_CREDIT: String = "$BASE_PATH/increase-credit"
