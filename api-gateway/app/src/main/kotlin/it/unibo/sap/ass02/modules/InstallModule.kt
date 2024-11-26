002ba87c8f847d7aede568df87713727587108c3
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-11-26 18:29:49 +0100

    feat(api): install micrometer metrics, create also metrics endpoint for prometheus

--- api-gateway/app/src/main/kotlin/it/unibo/sap/ass02/modules/InstallModule.kt
+++ api-gateway/app/src/main/kotlin/it/unibo/sap/ass02/modules/InstallModule.kt
@@ -3,8 +3,13 @@ package it.unibo.sap.ass02.modules
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.metrics.micrometer.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
 
 fun Application.installModule() {
     install(CORS) {
@@ -18,5 +23,17 @@ fun Application.installModule() {
     }
     install(ContentNegotiation) {
         json()
+    }
+}
+
+fun Application.prometheus() {
+    val appMicrometerRegistry = PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+    install(MicrometerMetrics) {
+        registry = appMicrometerRegistry
+    }
+    routing {
+        get("/metrics") {
+            call.respond(appMicrometerRegistry.scrape())
+        }
     }
 }
