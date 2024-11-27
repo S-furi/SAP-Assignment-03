d1f85067e81bb94823b5102d4487dc9eae816b82
S-furi <stefano.furi@studio.unibo.it>
2024-11-27 11:00:33 +0100

    chore(ride-service): add JsonUtil logging when something goes wrong

--- ride-service/core/src/main/kotlin/it/unibo/sap/ass02/infrastructure/util/JsonUtils.kt
+++ ride-service/core/src/main/kotlin/it/unibo/sap/ass02/infrastructure/util/JsonUtils.kt
@@ -3,9 +3,12 @@ package it.unibo.sap.ass02.infrastructure.util
 import io.ktor.client.statement.HttpResponse
 import io.ktor.client.statement.bodyAsText
 import kotlinx.serialization.json.Json
+import org.slf4j.LoggerFactory
 import java.nio.charset.StandardCharsets
 
 object JsonUtils {
+    val logger = LoggerFactory.getLogger(JsonUtils::class.java)
+
     val ignoreKeyJson =
         Json {
             ignoreUnknownKeys = true
@@ -15,6 +18,6 @@ object JsonUtils {
         runCatching {
             res.takeIf { it.status.value in 200..299 }?.let {
                 ignoreKeyJson.decodeFromString<T>(it.bodyAsText(StandardCharsets.UTF_8))
-            }
-        }.getOrNull()
+            }.also { if (it == null) logger.error("Got a status code different than [200-299]") }
+        }.onFailure { logger.error("An error occurred: ${it.message} \n${it.stackTraceToString()}") }.getOrNull()
 }
