a297515e4794631cd4026fbc275dff116e4edb2d
S-furi <stefano.furi@studio.unibo.it>
2024-11-27 11:02:25 +0100

    feat(ride-service): make resolver logic more robust

--- ride-service/service/src/main/kotlin/it/unibo/sap/ass02/service/routing/RideSimulationResolver.kt
+++ ride-service/service/src/main/kotlin/it/unibo/sap/ass02/service/routing/RideSimulationResolver.kt
@@ -2,26 +2,34 @@ package it.unibo.sap.ass02.service.routing
 
 import it.unibo.sap.ass02.domain.Ride
 import it.unibo.sap.ass02.infrastructure.RideProxy
+import org.slf4j.LoggerFactory
 
 object RideSimulationResolver {
+    private val logger = LoggerFactory.getLogger(RideSimulationResolver::class.java)
+
     private val registry = mutableMapOf<Int, Ride>()
 
-    private fun retrieveRide(id: Int) = RideProxy.getRide(id)
+    private fun retrieveRide(id: Int) =
+        RideProxy.getRide(id).also {
+            if (it == null) {
+                logger.error("Cannot retrieve Ride!")
+            }
+        }
 
-    fun findRide(id: Int) = registry[id] ?: retrieveRide(id)
+    fun findRide(id: Int): Ride? = registry[id] ?: retrieveRide(id)
 
     fun startRide(id: Int): Int? {
         if (registry[id] != null) {
             throw IllegalStateException("Ride with id $id has been already started!")
         }
-        return findRide(id)
+        return retrieveRide(id)
             ?.start()
             ?.also {
                 registry[it.id] = it
+                logger.debug("RIde Found, setting it inside registry")
             }?.id
     }
 
     fun stopRide(id: Int): Int? =
-        registry[id]?.also { it.end() }?.id
-            ?: throw IllegalStateException("Ride with id $id has not been started yet!")
+        registry[id].also { if (it == null) logger.error("Ride with id $id has not been started yet!") else it.end() }?.id
 }
