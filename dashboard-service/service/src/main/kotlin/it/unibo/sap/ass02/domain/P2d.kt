f74f9fe02691be305e4a46550d767a35a596e047
S-furi <stefano.furi@studio.unibo.it>
2024-12-03 15:15:39 +0100

    chore: remove P2D unused interface

--- dashboard-service/service/src/main/kotlin/it/unibo/sap/ass02/domain/P2d.kt
+++ dashboard-service/service/src/main/kotlin/it/unibo/sap/ass02/domain/P2d.kt
@@ -3,13 +3,7 @@ package it.unibo.sap.ass02.domain
 import kotlinx.serialization.Serializable
 
 @Serializable
-sealed interface P2d {
-    val x: Double
-    val y: Double
-}
-
-@Serializable
-data class P2dImpl(
-    override val x: Double,
-    override val y: Double,
-) : P2d
+data class P2d(
+    val x: Double,
+    val y: Double,
+)
