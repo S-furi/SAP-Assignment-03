10d4debf8b2d89ddd54d34a1828d7490e43f237e
S-furi <stefano.furi@studio.unibo.it>
2024-12-04 11:36:21 +0100

    fix(ride-registry): make DTO more concise

--- ride-registry/core/src/main/kotlin/it/unibo/sap/ass02/domain/Ride.kt
+++ ride-registry/core/src/main/kotlin/it/unibo/sap/ass02/domain/Ride.kt
@@ -3,10 +3,11 @@ package it.unibo.sap.ass02.domain
 import it.unibo.sap.ass02.domain.ddd.Entity
 import it.unibo.sap.ass02.domain.model.EBike
 import it.unibo.sap.ass02.domain.model.User
+import it.unibo.sap.ass02.utils.RideSerializer
 import kotlinx.serialization.Serializable
 import java.time.LocalDate
 
-@Serializable
+@Serializable(with = RideSerializer::class)
 sealed interface Ride : Entity<Int> {
     val ebike: EBike
     val user: User
