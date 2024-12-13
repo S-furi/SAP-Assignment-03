ae59aa48e8577ecd9da91ba6ca0c05079ced2c2b
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-13 17:59:01 +0100

    feat(kafka): add event consumer factory

--- vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/event/EventConsumer.java
+++ vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/event/EventConsumer.java
@@ -1,2 +1,40 @@
-package it.unibo.sap.ass02.demo.controller.event;public class EventConsumer {
+package it.unibo.sap.ass02.demo.controller.event;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class EventConsumer {
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServer;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    private String groupId;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        final Map<String, Object> configurationProps = new HashMap<>();
+        configurationProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, this.bootstrapServer);
+        configurationProps.put(ConsumerConfig.GROUP_ID_CONFIG, this.groupId);
+        configurationProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configurationProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(configurationProps);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListener() {
+        final ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(this.consumerFactory());
+        return factory;
+    }
 }
