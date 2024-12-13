be8d296acb42ba4bff467b68c0871d2fd8caa18e
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-13 17:59:44 +0100

    feat(kafka): add handler for updateLocationTopic, create sendMessage method

--- vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/event/MessageConsumer.java
+++ vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/event/MessageConsumer.java
@@ -1,2 +1,33 @@
-package it.unibo.sap.ass02.demo.controller.event;public class MessageConsumer {
+package it.unibo.sap.ass02.demo.controller.event;
+
+import it.unibo.sap.ass02.demo.controller.event.dto.Deserializers;
+import it.unibo.sap.ass02.demo.service.EBikeService;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class MessageConsumer {
+
+    private final EBikeService eBikeService;
+    private final static String EBIKEID_KEY= "ebikeId";
+    private final static  String X_POS_KEY = "x";
+    private final static  String Y_POS_KEY = "y";
+
+    public MessageConsumer(final EBikeService eBikeService){
+        this.eBikeService = eBikeService;
+    }
+    @KafkaListener(topics = "update-location")
+    public void listenUpdateLocation(final String message) {
+        Deserializers.deserializeUpdateLocationMessage(message,
+                MessageConsumer.EBIKEID_KEY,
+                MessageConsumer.X_POS_KEY,
+                MessageConsumer.Y_POS_KEY).ifPresent(messageDto-> {
+
+            this.eBikeService.updateLocation(
+                    messageDto.ebikeId(),
+                    messageDto.x(),
+                    messageDto.y());
+        });
+    }
 }
