0daf53df90f2e35d21244542b678d0570385589f
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-13 18:08:14 +0100

    feat: add UpdateBatteryMessageDTO

--- vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/event/dto/UpdateBatteryMessageDTO.java
+++ vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/event/dto/UpdateBatteryMessageDTO.java
@@ -1,2 +1,3 @@
-package it.unibo.sap.ass02.demo.controller.event.dto;public record UpdateBatteryMessageDTO() {
-}
+package it.unibo.sap.ass02.demo.controller.event.dto;
+
+public record UpdateBatteryMessageDTO(String ebikeId, Integer delta) { }
