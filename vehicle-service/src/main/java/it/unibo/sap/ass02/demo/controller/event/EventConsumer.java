1cd94198e73ee796af1e62e1bd8a235fb7879240
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-14 16:29:05 +0100

    refactor: remove unused import

--- vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/event/EventConsumer.java
+++ vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/event/EventConsumer.java
@@ -2,7 +2,6 @@ package it.unibo.sap.ass02.demo.controller.event;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
