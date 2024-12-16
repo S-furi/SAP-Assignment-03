89a8503bd600f0be1de2b9cd80789f1158726d0a
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-16 15:49:06 +0100

    fix: range values, set from 0 to 100

--- a-bike/a-bike.py
+++ a-bike/a-bike.py
@@ -46,7 +46,7 @@ class AutonomousBike:
         # Query the system and get the informations of all the stations available
         # in the 30km range by using the a-bike position
         # check all the available stations with a radius from 0 to 100km
-        for radius in range(0, 100, 5):
+        for radius in range(0, 101, 5):
             possible_stations = World.get_stations(self.x, self.y, radius)
             # return the first available list of station
             if len(possible_stations) > 0:
