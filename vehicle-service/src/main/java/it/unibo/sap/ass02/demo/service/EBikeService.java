b53838e605be123ff9a48d58b84a3757d921f9a7
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-18 14:03:26 +0100

    feat: add EventRepository inside the EBikeService

--- vehicle-service/src/main/java/it/unibo/sap/ass02/demo/service/EBikeService.java
+++ vehicle-service/src/main/java/it/unibo/sap/ass02/demo/service/EBikeService.java
@@ -1,5 +1,6 @@
 package it.unibo.sap.ass02.demo.service;
 
+import it.unibo.sap.ass02.demo.controller.event.sourcing.EventRepository;
 import it.unibo.sap.ass02.demo.domain.EBike;
 import it.unibo.sap.ass02.demo.domain.EBikeImpl;
 import it.unibo.sap.ass02.demo.domain.P2d;
@@ -8,17 +9,20 @@ import it.unibo.sap.ass02.demo.repositories.P2dRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Service
 public class EBikeService {
     private final EBikeRepository eBikeRepository;
     private final P2dRepository p2dRepository;
+    private final EventRepository eventRepository;
 
     @Autowired
-    public EBikeService(final EBikeRepository eBikeRepository, final P2dRepository p2dRepository) {
+    public EBikeService(final EBikeRepository eBikeRepository, final P2dRepository p2dRepository, final EventRepository repo) {
         this.eBikeRepository = eBikeRepository;
         this.p2dRepository = p2dRepository;
+        this.eventRepository = repo;
     }
 
     public EBikeImpl createNewEBike(EBikeImpl eBike) {
@@ -29,6 +33,7 @@ public class EBikeService {
                     .findByXAndY(location.x(), location.y())
                     .orElseGet(() -> this.p2dRepository.save(location));
             eBike.updateLocation(savedLocation);
+            this.eventRepository.createNewEBike(eBike.getID(), eBike.getLocation());
             return this.eBikeRepository.save(eBike);
         }else{
             throw new IllegalArgumentException("The input ebike ID already exists");
@@ -43,6 +48,7 @@ public class EBikeService {
                                             .orElseGet(() -> this.p2dRepository.save(new P2d(xPos, yPos)));
                     ebike.updateLocation(newPosition);
                     this.eBikeRepository.save(ebike);
+                    this.eventRepository.updateEBikePosition(ebikeId, new P2d(xPos, yPos));
                 });
     }
 
@@ -69,11 +75,22 @@ public class EBikeService {
     }
 
     public Iterable<EBikeImpl> getAllEBike() {
-        return this.eBikeRepository.findAll();
+        final Iterable<EBikeImpl> eBikes = this.eBikeRepository.findAll();
+        eBikes.forEach(ebike -> {
+            this.eventRepository.getLatestPosition(ebike.getID()).ifPresent(ebike::updateLocation);
+        });
+        return  eBikes;
     }
 
     public Optional<EBikeImpl> getEBikeById(final String id) {
-        return this.eBikeRepository.findById(id);
+        final Optional<EBikeImpl> optEBike = this.eBikeRepository.findById(id);
+        final Optional<P2d> latestPosition = this.eventRepository.getLatestPosition(id);
+        latestPosition.ifPresent(position -> {
+            optEBike.ifPresent(ebike -> {
+                ebike.updateLocation(position);
+            });
+        });
+        return optEBike;
     }
 
     public boolean existsById(final String id) {
