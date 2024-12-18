f3a25f4e91eb6ccb9508b65cfd8b8394a44aa837
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-18 14:01:36 +0100

    feat: add Event class

--- vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/event/sourcing/events/Event.java
+++ vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/event/sourcing/events/Event.java
@@ -1,8 +1,13 @@
-package it.unibo.sap.ass02.demo.controller.event.sourcing;
+package it.unibo.sap.ass02.demo.controller.event.sourcing.events;
+
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 import java.util.UUID;
 
+@Getter
+@Setter
 public class Event {
     public final UUID id = UUID.randomUUID();
     public final Date eventDate = new Date();
