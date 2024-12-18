eaf539be3f633578251edbf3d981662807efe71b
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-12-18 14:01:20 +0100

    feat: add new event, NewEbikePosition event

--- vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/event/sourcing/events/NewEBikePositionEvent.java
+++ vehicle-service/src/main/java/it/unibo/sap/ass02/demo/controller/event/sourcing/events/NewEBikePositionEvent.java
@@ -1,2 +1,14 @@
-package it.unibo.sap.ass02.demo.controller.event.sourcing.events;public class NewEBikePositionEvent {
+package it.unibo.sap.ass02.demo.controller.event.sourcing.events;
+
+import it.unibo.sap.ass02.demo.domain.P2d;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class NewEBikePositionEvent extends Event{
+    private String ebikeId;
+    private P2d newPosition;
 }
