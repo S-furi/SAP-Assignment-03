f197b639fdcc3a3fccc3cfcb2b64f1fb717f4286
S-furi <stefano.furi@studio.unibo.it>
2025-03-17 19:07:09 +0100

    build: make build script accept deletion

--- kube-migration/run_all.sh
+++ kube-migration/run_all.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-find . -path "*/docker" -prune -o -name "*.yaml" -print | xargs -n 1 kubectl apply -f
+
+if [[ "$1" != "apply" && "$1" != "delete" ]]; then
+  echo "Usage: $0 [apply|delete]"
+  exit 1
+fi
+
+ACTION=$1
+
+find . -path "*/docker" -prune -o -name "*.yaml" -print | xargs -n 1 kubectl $ACTION -f
+
