5fb85e36a9229c90364901274fe2062f2b7d96ea
S-furi <stefano.furi@studio.unibo.it>
2025-03-24 10:41:13 +0100

    chore: remove unused k8s deployments

--- api-gateway/k8s-deployment/deploy-api-gateway.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-kubectl apply -f ./config.yaml
-kubectl apply -f ./services.yaml
-kubectl apply -f ./deployments.yaml
-kubectl apply -f ./prometheus-config.yaml
-kubectl apply -f ./grafana-config.yaml
